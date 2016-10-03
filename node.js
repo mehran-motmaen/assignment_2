ecf494434f520528689c41ad1bdfcc87418851b9
mehran motmaen <mehran motmaen>
2016-10-03 11:23:59 +0330

    Initial commit

---
+++ node.js
@@ -0,0 +1,22 @@
+/**
+ * Created by VAIO on 10/3/2016.
+ */
+
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (req, res) {
+    displayForm(res);
+});
+
+function displayForm(res) {
+    fs.readFile('html.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+
+server.listen(8080);
